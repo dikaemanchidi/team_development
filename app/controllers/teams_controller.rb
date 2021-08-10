7fe18ac256e91b4dd4d84e8b722002d47fbcadab
dikaemanchidi <dikaemanchidi@gmail.com>
2021-08-11 01:18:54 +0900

    message

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,5 @@
 class TeamsController < ApplicationController
-  before_action :current_user
   before_action :authenticate_user!
-  # before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :set_team, only: %i[show edit update destroy]
 
   def index
@@ -14,38 +12,35 @@ class TeamsController < ApplicationController
   end
 
   def new
-    @team=Team.new
+    @team = Team.new
   end
 
-  def edit
-    unless current_user.id == @team.owner_id
-    redirect_to team_url(params[:id ]), notice: "Access Denied!"
-  end
- end
+  def edit; end
+
   def create
     @team = Team.new(team_params)
     @team.owner = current_user
     if @team.save
       @team.invite_member(@team.owner)
-      redirect_to @team, notice: I18n.t('views.messages.create_team')
+      redirect_to @team, notice: 'チーム作成に成功しました！'
     else
-      flash.now[:error]=I18n.t('views.messages.failed_to_save_team')
+      flash.now[:error] = '保存に失敗しました、、'
       render :new
     end
   end
 
   def update
     if @team.update(team_params)
-      redirect_to @team, notice: I18n.t('views.messages.update_team')
+      redirect_to @team, notice: 'チーム更新に成功しました！'
     else
-      flash.now[:error] = I18n.t('views.messages.failed_to_save_team')
+      flash.now[:error] = '保存に失敗しました、、'
       render :edit
     end
   end
 
   def destroy
     @team.destroy
-    redirect_to teams_url, notice: I18n.t('views.messages.delete_team')
+    redirect_to teams_url, notice: 'チーム削除に成功しました！'
   end
 
   def dashboard
@@ -58,14 +53,6 @@ class TeamsController < ApplicationController
     @team = Team.friendly.find(params[:id])
   end
 
-
-  # def require_same_user
-  #   if current_user.id != @team.owner_id
-  #   # if current_user != @team.user && !team.owner?
-  #   flash[:alert] = "You can only edit or delete your own article"
-  #   redirect_to teams_url, notice: I18n.t('views.messages.delete_team')
-  #   end
-  # end
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
   end
